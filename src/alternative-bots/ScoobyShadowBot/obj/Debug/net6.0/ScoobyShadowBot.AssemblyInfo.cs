0a571b3e767dfb8f0d7f18a1f3bc44a1db3da17b
jovan196 <19623032@std.stei.itb.ac.id>
2025-03-23 09:42:55 +0700

    chore: rename bot Roma3000 to ScoobyRoma3000

--- src/alternative-bots/ScoobyShadowBot/obj/Debug/net6.0/ScoobyShadowBot.AssemblyInfo.cs
+++ src/alternative-bots/ScoobyShadowBot/obj/Debug/net6.0/ScoobyShadowBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScoobyShadowBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f771047de0e069ef393359f06aa3930736cd7f86")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScoobyShadowBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScoobyShadowBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/main-bot/ScoobyGodBot/obj/Debug/net6.0/ScoobyGodBot.AssemblyInfo.cs
+++ src/main-bot/ScoobyGodBot/obj/Debug/net6.0/ScoobyGodBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScoobyGodBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f771047de0e069ef393359f06aa3930736cd7f86")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScoobyGodBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScoobyGodBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
